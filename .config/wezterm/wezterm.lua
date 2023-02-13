fe3238e5d7f4b91957b8d917ae955b6560d95fe5
Fabien Engels <code@fabienengels.com>
2023-02-13 09:39:33 +0100

    Add wezterm settings

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+
+return {
+  font = wezterm.font 'JetBrains Mono',
+  enable_tab_bar = false,
+  color_scheme = 'Mashup Colors (terminal.sexy)',
+  window_background_opacity = 0.9,
+  -- default_prog = { 'fish' }
+}
