776374954ce883bfb6f5851090f8e4db836d7b54
Fabien Engels <code@fabienengels.com>
2023-04-19 16:57:43 +0200

    Remove font directive from wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,6 @@
-local wezterm = require("wezterm")
-
 return {
-	font = wezterm.font("JetBrains Mono"),
 	font_size = 13,
 	enable_tab_bar = false,
-	-- https://wezfurlong.org/wezterm/colorschemes/index.html
 	color_scheme = "kanagawabones",
 	window_background_opacity = 0.9,
 	default_prog = { "fish" },
