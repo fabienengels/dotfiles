37c9c7d293714f65029be212e370f35272a80347
Fabien Engels <code@fabienengels.com>
2023-05-04 15:37:58 +0200

    Allow override of wezterm settings via override.lua module

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-return {
+local config = {
 	font_size = 13,
 	enable_tab_bar = false,
 	color_scheme = "kanagawabones",
@@ -6,3 +6,11 @@ return {
 	default_prog = { "fish" },
 	allow_square_glyphs_to_overflow_width = "Always",
 }
+
+local has_override, override = pcall(require, "override")
+
+if has_override then
+  for k,v in pairs(override) do config[k] = v end
+end
+
+return config
