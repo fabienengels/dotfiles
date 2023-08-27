2f1835da10c5f21713693fecc45a7dea7e28c047
Fabien Engels <code@fabienengels.com>
2023-08-27 13:44:53 +0200

    Update wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,13 @@
 local config = {
-	font_size = 13,
-	enable_tab_bar = false,
-	color_scheme = "kanagawabones",
-	window_background_opacity = 0.9,
-	default_prog = { "fish" },
-	allow_square_glyphs_to_overflow_width = "Always",
+  font_size = 13,
+  color_scheme = "kanagawabones",
+  hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = 0.9,
+  default_prog = { "fish" },
+  allow_square_glyphs_to_overflow_width = "Always",
+  native_macos_fullscreen_mode = true,
+  audible_bell = "Disabled",
+  pane_focus_follows_mouse = true
 }
 
 local has_override, override = pcall(require, "override")
