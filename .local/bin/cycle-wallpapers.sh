76bd27c9eac8be2f001fdefd957e9519d3b31d82
Fabien Engels <code@fabienengels.com>
2024-10-14 13:52:21 +0200

    Fix bash shebang by using /usr/bin/env (NixOS)

--- .local/bin/cycle-wallpapers.sh
+++ .local/bin/cycle-wallpapers.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 while sleep $1; do random-wallpaper.sh; done
--- .local/bin/random-wallpaper.sh
+++ .local/bin/random-wallpaper.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for i in $(find ~/Sync/Pictures/Wallpapers -type f | shuf); do
   swww img $i && echo $i && break
