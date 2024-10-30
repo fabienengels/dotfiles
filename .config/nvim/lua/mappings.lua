8765990ebea8ea8e493832dff86dfab01678594a
Fabien Engels <code@fabienengels.com>
2024-10-30 11:01:04 +0100

    Start to remap neovim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,7 +1,5 @@
 require "nvchad.mappings"
 
---
-
 local map = vim.keymap.set
 
 -- Indent with Tab
@@ -13,44 +11,22 @@ map("v", "<S-Tab>", "<<<Esc>gv", { noremap = true, silent = true })
 -- Stop search highlight
 map("n", "<ESC>", ":noh<return><ESC>", { noremap = true, silent = true })
 
--- Theme picker
--- map("n", "<leader>tp", "<cmd>lua require('nvchad.themes').open()<cr>", { noremap = true, silent = true })
+-- Buffers
+map("n", "<C-N>", ":bnext<cr>", { noremap = true, silent = true })
+map("n", "<C-P>", ":bprev<cr>", { noremap = true, silent = true })
 
+-- Telescope
+map("n", "<leader>fg", "<cmd>Telescope git_files<cr>", { noremap = true, silent = true })
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true, silent = true })
 
--- https://github.com/fabienengels/dotfiles/blob/47cec2c1cff853fac0c3f20c954243b124effa6f/.config/nvim/lua/plugins/shortcuts.lua
---   {
---     "folke/which-key.nvim",
---     config = function ()
---       require("which-key").register({
---         ["w"] = { "<cmd>w!<cr>", "Save" },
---         ["q"] = { "<cmd>q!<cr>", "Quit" },
---         ["c"] = { "<cmd>BufferKill<cr>", "Close Buffer" },
---         b = {
---           name = "Buffers",
---           f = { "<cmd>Telescope buffers<cr>", "Find buffer" },
---         },
---         f = {
---           name = "Files",
---           g = { "<cmd>Telescope git_files<cr>", "Find git file" }, -- create a binding with label
---           f = { "<cmd>Telescope find_files<cr>", "Find file" }, -- create a binding with label
---           r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" }, -- additional options for creating the keymap
---           n = { "New File" }, -- just a label. don't create any mapping
---           e = "Edit File", -- same as above
---         },
---         g = {
---           name = "Git",
---           j = { "<cmd>Gitsigns next_hunk<cr>", "Next hunk" },
---           k = { "<cmd>Gitsigns prev_hunk<cr>", "Previous hunk" },
---           p = { "<cmd>Gitsigns preview_hunk<cr>", "Preview hunk" },
---           r = { "<cmd>Gitsigns reset_hunk<cr>", "Reset hunk" },
---           R = { "<cmd>Gitsigns reset_buffer<cr>", "Reset buffer" },
---           s = { "<cmd>Gitsigns stage_hunk<cr>", "Stage hunk" },
---           u = { "<cmd>Gitsigns undo_stage_hunk<cr>", "Undo stage hunk" },
---           o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
---           b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
---           c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
---           d = { "<cmd>Gitsigns diffthis<cr>", "Git diff" },
---         },
+-- Git
+map("n", "<leader>gj", "<cmd>Gitsigns next_hunk<cr>", { noremap = true, silent = true })
+map("n", "<leader>gk", "<cmd>Gitsigns prev_hunk<cr>", { noremap = true, silent = true })
+map("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", { noremap = true, silent = true })
+map("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", { noremap = true, silent = true })
+map("n", "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>", { noremap = true, silent = true })
+map("n", "<leader>gs", "<cmd>Gitsigns stage_hunk<cr>", { noremap = true, silent = true })
+map("n", "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr>", { noremap = true, silent = true })
 --         d = {
 --           name = "Diagnostics",
 --           -- j = { "<cmd>Lspsaga diagnostic_jump_next<cr>", "Next diagnostic" },
@@ -77,21 +53,3 @@ map("n", "<ESC>", ":noh<return><ESC>", { noremap = true, silent = true })
 --           t = { "<cmd>TSConfigInfo<cr>", "TreeSitter information" },
 --           l = { "<cmd>LspInfo<cr>", "LSP information" },
 --         },
---       }, { prefix = "<leader>" })
---
---       -- TODO
---
---       -- Move around splits
---       vim.api.nvim_set_keymap("n", "<C-J>", "<C-W>j", { noremap = true, silent = true })
---       vim.api.nvim_set_keymap("n", "<C-K>", "<C-W>k", { noremap = true, silent = true })
---       vim.api.nvim_set_keymap("n", "<C-L>", "<C-W>l", { noremap = true, silent = true })
---       vim.api.nvim_set_keymap("n", "<C-H>", "<C-W>h", { noremap = true, silent = true })
---
---       -- Move around buffers
---       vim.api.nvim_set_keymap("n", "<C-N>", ":bnext<cr>", { noremap = true, silent = true })
---       vim.api.nvim_set_keymap("n", "<C-P>", ":bprev<cr>", { noremap = true, silent = true })
---
---       -- Close current window
---       vim.api.nvim_set_keymap("n", "<C-C>", ":close<CR>", { noremap = true, silent = true })
---     end
---   }
