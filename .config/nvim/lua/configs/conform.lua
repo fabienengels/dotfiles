0cab2fea956316371a1186918bd165ddc3ec7bd6
Fabien Engels <code@fabienengels.com>
2024-12-16 11:28:11 +0100

    [neovim] Enable auto formatting viw conform

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,15 +1,17 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    -- ts = { "deno_fmt" },
+    rust = { "rustfmt" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
