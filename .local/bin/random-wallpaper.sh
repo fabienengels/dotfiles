b5881a12fb255feab76376f5e058ee3b984623a4
Fabien Engels <code@fabienengels.com>
2026-03-26 19:55:15 +0100

    Use matugen helix theme and increase matugen contrast

--- .local/bin/random-wallpaper.sh
+++ .local/bin/random-wallpaper.sh
@@ -3,6 +3,7 @@
 for i in $(find ~/Sync/Pictures/Wallpapers -type f | shuf); do
   echo $i
   awww img $i
-  matugen image --show-colors $i
+  # matugen image --show-colors $i
+  matugen image --prefer saturation --show-colors --contrast 1 $i
   break
 done
