a18771d44d4817d802243523ec37b78232eae0b0
Fabien Engels <code@fabienengels.com>
2026-05-26 10:06:22 +0200

    Show relative path in lualine

--- .config/nvim/lua/pack.lua
+++ .config/nvim/lua/pack.lua
@@ -98,6 +98,12 @@ require("lualine").setup {
   options = {
     theme = "catppuccin-nvim",
   },
+  sections = {
+    lualine_c = { {
+      "filename",
+      path = 1,
+    } },
+  },
 }
 
 require("gitsigns").setup()
