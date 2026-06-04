1912ca7771c1586cd8ddcada56ae02decebb76b4
Fabien Engels <code@fabienengels.com>
2026-06-04 09:12:18 +0200

    Cleanup

--- .config/nvim/lua/pack.lua
+++ .config/nvim/lua/pack.lua
@@ -69,9 +69,6 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = languages,
   callback = function()
     vim.treesitter.start() -- highlighting
-    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'     -- folds
-    -- vim.wo.foldmethod = 'expr'
-    -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
   end,
 })
 
