4c6e9bbbb3bad5765c1ac530b55e71db15cbef9f
Fabien Engels <code@fabienengels.com>
2026-07-21 13:38:45 +0200

    Format all minimax.nvim files

--- .config/nvim-minimax/init.lua
+++ .config/nvim-minimax/init.lua
@@ -73,10 +73,10 @@ _G.Config = {}
 -- - `:h autocommand`
 -- - `:h nvim_create_augroup()`
 -- - `:h nvim_create_autocmd()`
-local gr = vim.api.nvim_create_augroup('custom-config', {})
+local gr = vim.api.nvim_create_augroup("custom-config", {})
 Config.new_autocmd = function(event, pattern, callback, desc)
-  local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
-  vim.api.nvim_create_autocmd(event, opts)
+	local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+	vim.api.nvim_create_autocmd(event, opts)
 end
 
 -- Define custom `vim.pack.add()` hook helper. Plugin data is passed as
@@ -85,19 +85,23 @@ end
 -- If any plugin requires installation hooks, add them after this function
 -- and before the first `vim.pack.add()` call.
 Config.on_packchanged = function(plugin_name, kinds, callback, desc)
-  local f = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then return end
-    if not ev.data.active then vim.cmd.packadd(plugin_name) end
-    callback(ev.data)
-  end
-  Config.new_autocmd('PackChanged', '*', f, desc)
+	local f = function(ev)
+		local name, kind = ev.data.spec.name, ev.data.kind
+		if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then
+			return
+		end
+		if not ev.data.active then
+			vim.cmd.packadd(plugin_name)
+		end
+		callback(ev.data)
+	end
+	Config.new_autocmd("PackChanged", "*", f, desc)
 end
 
 -- 'mini.nvim' - all-in-one plugin powering most MiniMax features.
 -- See 'plugin/30_mini.lua' for how it is used.
 -- Load now to have 'mini.misc' available for custom loading helpers.
-vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
+vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
 
 -- Loading helpers used to organize config into fail-safe parts. Example usage:
 -- - `now` - execute immediately. Use for what must be executed during startup.
@@ -115,9 +119,17 @@ vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
 -- See also:
 -- - `:h MiniMisc.safely()`
 -- - 'plugin/30_mini.lua' and 'plugin/40_plugins.lua'
-local misc = require('mini.misc')
-Config.now = function(f) misc.safely('now', f) end
-Config.later = function(f) misc.safely('later', f) end
+local misc = require("mini.misc")
+Config.now = function(f)
+	misc.safely("now", f)
+end
+Config.later = function(f)
+	misc.safely("later", f)
+end
 Config.now_if_args = vim.fn.argc(-1) > 0 and Config.now or Config.later
-Config.on_event = function(ev, f) misc.safely('event:' .. ev, f) end
-Config.on_filetype = function(ft, f) misc.safely('filetype:' .. ft, f) end
+Config.on_event = function(ev, f)
+	misc.safely("event:" .. ev, f)
+end
+Config.on_filetype = function(ft, f)
+	misc.safely("filetype:" .. ft, f)
+end
