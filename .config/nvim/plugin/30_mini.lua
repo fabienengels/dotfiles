52e7bde4b3e25ca82d9319f647875708fb9d7a63
Fabien Engels <code@fabienengels.com>
2026-08-13 16:03:43 +0200

    Disable MiniMisc.setup_auto_root()

--- .config/nvim/plugin/30_mini.lua
+++ .config/nvim/plugin/30_mini.lua
@@ -233,12 +233,6 @@ now_if_args(function()
 	-- Makes `:h MiniMisc.put()` and `:h MiniMisc.put_text()` public
 	require("mini.misc").setup()
 
-	-- Change current working directory based on the current file path. It
-	-- searches up the file tree until the first root marker ('.git' or 'Makefile')
-	-- and sets their parent directory as a current directory.
-	-- This is helpful when simultaneously dealing with files from several projects.
-	MiniMisc.setup_auto_root()
-
 	-- Restore latest cursor position on file open
 	MiniMisc.setup_restore_cursor()
 
